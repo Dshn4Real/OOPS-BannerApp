3513db4cbb621cbdb616625c71a7357e39b1f0dc
Dshn4Real <dishaun.jose07@gmail.com>
2026-02-17 10:57:06 +0530

    Clean up comments and documentation in OOPSBannerApp
    Removed unnecessary comments and documentation.

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,17 +1,7 @@
-/**
- * OOPSBannerApp
- *
- * <p>This application prints "OOPS" as a large
- * ASCII banner using '*' and spaces.</p>
- *
- * @author dishaun
- * @version 2.0
- */
+
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-
-        // 7 lines height, 9 characters width per letter
 
         System.out.println("  *****   " + "  *****   " + " *******  " + " ******* ");
         System.out.println(" *     *  " + " *     *  " + " *     *  " + " *     *");
