e2e3d4ec52257f102ff1935392d9a93cd65a5f29
Dshn4Real <dishaun.jose07@gmail.com>
2026-02-17 10:56:39 +0530

    Clean up comments in OOPSBannerApp.java
    Removed unnecessary comments and documentation.

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,20 +1,5 @@
-/**
- * OOPSBannerApp
- *
- * <p>This application prints the text "OOPS" to the console.
- * It demonstrates basic Java program structure including
- * class definition, main method, and console output.</p>
- *
- * @author dishaun
- * @version 1.0
- */
-public class OOPSBannerApp {
 
-    /**
-     * Entry point of the application.
-     *
-     * @param args Command-line arguments (not used)
-     */
+public class OOPSBannerApp {
     public static void main(String[] args) {
 
         // Print OOPS to console
