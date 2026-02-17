91d8c05064a527bced8f5ead7fe855a0b2740c38
DishaunJose <dishaun.jose07@gmail.com>
2026-02-17 10:32:01 +0530

    UC2 - Banner implementation

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,23 +1,24 @@
 /**
  * OOPSBannerApp
  *
- * <p>This application prints the text "OOPS" to the console.
- * It demonstrates basic Java program structure including
- * class definition, main method, and console output.</p>
+ * <p>This application prints "OOPS" as a large
+ * ASCII banner using '*' and spaces.</p>
  *
  * @author dishaun
- * @version 1.0
+ * @version 2.0
  */
 public class OOPSBannerApp {
 
-    /**
-     * Entry point of the application.
-     *
-     * @param args Command-line arguments (not used)
-     */
     public static void main(String[] args) {
 
-        // Print OOPS to console
-        System.out.println("OOPS");
+        // 7 lines height, 9 characters width per letter
+
+        System.out.println("  *****   " + "  *****   " + " *******  " + " ******* ");
+        System.out.println(" *     *  " + " *     *  " + " *     *  " + " *     *");
+        System.out.println("*       * " + "*       * " + "*       * " + "*      ");
+        System.out.println("*       * " + "*       * " + "*******   " + " ****** ");
+        System.out.println("*       * " + "*       * " + "*         " + "       *");
+        System.out.println(" *     *  " + " *     *  " + "*         " + "*      *");
+        System.out.println("  *****   " + "  *****   " + "*         " + " ****** ");
     }
 }
