9ef43a9417a84abc778b85f7602b90c83706f7e3
Dshn4Real <dishaun.jose07@gmail.com>
2026-02-17 10:57:43 +0530

    Remove comments from OOPSBannerApp.java
    Remove outdated comments from OOPSBannerApp.java.

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,4 @@
-/**
- * OOPSBannerApp UC3 – OOPS Banner Application (Use Case 3)
- *
- * This class improves UC2 by using String.join() instead of
- * string concatenation with + operator for better efficiency
- * and readability.
- *
- * @author dishaun
- * @version 3.0
- */
+
 
 public class OOPSBannerApp {
 
